ac91c863d4b766eb0c62000b5aac794ac1357d21
browk876 <browk876@newschool.edu>
2023-09-18 02:26:58 -0400

    c VIII

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ const logoPic = document.getElementById('LogoPic');
 const myPics = ['lenape.jpg', 'dallas.jpg', 'elsegundo.jpg', 'naperville.jpg', 'mountainview.jpg'];
 let picNumber = 1;
 
-function changeImage() {
+function changePic() {
     pic.src = MyPicSrc;
     picNumber = (picNumber + 1) % myPics.length;
-    const newImageSrc = myPics[picNumber];
-
-
+    const MyPicSrc = myPics[picNumber];
 }
-
+imageContainer.addEventListener('click', changePic);
