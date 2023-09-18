65547eeca001d5e6d8f9795cb70856b6da1fbb6c
browk876 <browk876@newschool.edu>
2023-09-18 01:56:08 -0400

    commit V

---
+++ script.js
@@ -0,0 +1,15 @@
+body {
+    margin: 0;
+    padding: 0;
+    background-color: #ADD8E6;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    height: 150vh;
+}
+h1 {
+    text-align: center;
+    color: rgb(0, 0, 0);
+    margin-bottom: 10px; 
+}
